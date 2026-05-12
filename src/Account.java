70a740bab2435a3ae3df28c90699cb20fde8b97b
Paulo Ricardo <pauloricardoso.dev@gmail.com>
2026-05-12 19:35:03 -0300

    add multi-account support and account switching

--- src/Account.java
+++ src/Account.java
@@ -1,6 +1,32 @@
+import java.util.ArrayList;
+
 public class Account {
 
-double balance = 1000;
+    private int accountNumber;
+
+    private double balance;
+
+    private ArrayList<String> transactionHistory = new ArrayList<>();
+
+public Account(int accountNumber, double intialBalance) {
+
+    this.accountNumber = accountNumber;
+    balance = intialBalance;
+}
+
+public int getAccountNumber() {
+    return accountNumber;
+}
+
+public double getBalance(){
+    return balance;
+}
+
+public void showAccountInfo() {
+
+    System.out.println("Account Number: " + accountNumber);
+    System.out.println("Current Balance: $" + balance);
+}
 
 public void checkBalance() {
     System.out.println("Current balance: $" + balance);
@@ -8,6 +34,8 @@ public void checkBalance() {
 public void deposit(double amount) {
 
     balance += amount;
+
+    transactionHistory.add("Deposit: $" + amount);
 
     System.out.println("Deposit successful!");
 }
@@ -21,8 +49,18 @@ public void withdraw(double amount) {
 
         balance -= amount;
 
+        transactionHistory.add("Withdraw: $" + amount);
+
         System.out.println("Withdrawal successful!");
     }
 }
 
+public void showTransactionHistory() {
+
+        System.out.println("\n=== Transaction History ===");
+
+        for (String transaction : transactionHistory) {
+            System.out.println(transaction);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    public static Account findAccount(ArrayList<Account> accounts, int accountNumber) {
+
+        for (Account account : accounts) {
+
+            if (account.getAccountNumber() == accountNumber) {
+                return account;
+            }
+        }
+
+        return null;
+    }
 
     public static void checkBalance(double balance) {
         System.out.println("Current Balance: $" + balance);
@@ -33,19 +47,26 @@ public class Main {
 
     public static void showMenu() {
 
-        System.out.println("\n=== BANK SYSTEM ===");
-        System.out.println("1 - Check Balance");
-        System.out.println("2 - Deposit");
-        System.out.println("3 - Withdraw");
-        System.out.println("4 - Exit");
-        System.out.print("Choose an option: ");
     }
 
     public static void main(String[] args) {
 
-        Account account = new Account();
+        ArrayList<Account> accounts = new ArrayList<>();
+
+        accounts.add(new Account (12345, 1000));
+        accounts.add(new Account(67890, 500));
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter account number");
+        int accountNumber = scanner.nextInt();
+
+        Account account = findAccount(accounts, accountNumber);
+
+        if (account == null) {
+            System.out.println("Account not found!");
+            return;
+        }
 
         int option;
 
@@ -53,32 +74,75 @@ public class Main {
 
             showMenu();
 
+            System.out.println("\n=== BANK SYSTEM ===");
+            System.out.println("1 - Create Account");
+            System.out.println("2 - Switch Account");
+            System.out.println("3 - Check Balance");
+            System.out.println("4 - Deposit");
+            System.out.println("5 - Withdraw");
+            System.out.println("6 - Transaction History");
+            System.out.println("7 - Exit");
+            System.out.println("Choose an option: ");
+
             option = scanner.nextInt();
 
             switch (option) {
 
                 case 1:
-                    account.checkBalance();
+                    System.out.println("Enter new account number: ");
+                    int newAccountNumber = scanner.nextInt();
+
+                    System.out.println("Enter initial balance");
+                    double initialBalance = scanner.nextDouble();
+
+                    accounts.add(new Account(newAccountNumber, initialBalance));
+
+                    System.out.println("Account created successfully!");
                     break;
 
                 case 2:
 
+                    System.out.println("Enter account number: ");
+                    int switchAccountNumber = scanner.nextInt();
+
+                    Account foundAccount = findAccount(accounts, switchAccountNumber);
+
+                    if (foundAccount != null) {
+
+                        account = foundAccount;
+
+                        System.out.println("Account switched successfully");
+
+                    } else {
+
+                        System.out.println("Account not found!");
+                    }
+                    break;
+
+                case 3:
+                    account.checkBalance();
+                    break;
+
+                case 4:
                     System.out.print("Enter deposit amount: ");
                     double depositAmount = scanner.nextDouble();
 
                     account.deposit(depositAmount);
-
                     break;
 
-                case 3:
+                case 5:
                     System.out.println("Enter withdrawal amount");
                     double withdrawAmount = scanner.nextDouble();
 
                     account.withdraw(withdrawAmount);
-
                     break;
 
-                case 4:
+
+                case 6:
+                    account.showTransactionHistory();
+                    break;
+
+                case 7:
                     System.out.println("Exiting system...");
                     break;
 
@@ -87,7 +151,7 @@ public class Main {
 
             }
 
-        } while (option != 4);
+        } while (option != 7);
 
         scanner.close();
     }
